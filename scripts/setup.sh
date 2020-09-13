d05755234f580f51877a3c424a3ea1176df002fc
DevFJD <fduenkel@outlook.com>
2020-09-13 08:21:44 +0200

    init commit

---
+++ scripts/setup.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Create folders
+mkdir -p ~/Developer/code/go
+mkdir -p ~/Developer/code/mystuff
+mkdir -p ~/Developer/code/lab
+
+
+# Keep sudo alive
+sudo -v
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
+
+# Install xcode CLI
+xcode-select —-install
+
+# Install Homebrew
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Homebrew installed successfully"
+else
+    echo "Homebrew already installed!"
+fi
+
+# Install Packages
+PACKAGES=(
+    zsh
+    starship
+    tmux
+    python
+    go
+    nvm
+    htop
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+# Install Casks
+echo "Installing cask..."
+CASKS=(
+    iterm2
+    insomnia
+    robo-3t
+    sequel-pro
+    visual-studio-code
+    gitkraken
+    docker
+    firefox
+    microsoft-edge
+    spotify
+    whatsapp
+)
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+# Remove outdated versions from the cellar.
+echo "Running brew cleanup..."
+brew cleanup
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
